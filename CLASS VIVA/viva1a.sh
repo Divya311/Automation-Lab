3163e24d3a599141cac153165b0e033b8c329787
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-09-29 16:55:19 +0530

    archiving cron logs

--- CLASS VIVA/viva1a.sh
+++ CLASS VIVA/viva1a.sh
@@ -1,1 +1,17 @@
 # Logging all logs of cron scheduler and archiving them
+
+#bin/bash
+
+
+
+more /var/log/syslog | grep "CRON" > /home/kshitiz/cron.txt
+
+cd /home/kshitiz
+
+
+tar -cf cron.tar cron.txt
+
+rm cron.txt
+
+
+ 
