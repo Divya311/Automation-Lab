43a1aed72be9036abcd135084ca17f8ef9fd1070
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-09-29 17:03:06 +0530

    deleting file of same extension

--- Shell Scripting/deleting_file_of_same_extension.sh
+++ Shell Scripting/deleting_file_of_same_extension.sh
@@ -1,1 +1,26 @@
 # deleting file of same extension
+
+#bin/bash
+
+read -e -p "PATH 1 : " path1
+read -e -p "PATH 2 : " path2
+read -e -p "EXTENSION : " ext
+
+echo "Files in path1 : "
+ls $path1/*.$ext
+
+echo "Files in path2 : "
+ls $path2/*.$ext
+
+rm $path1/*.$ext
+
+cd $path2
+
+tar -cf archive.tar *.$ext
+
+read -e -p "BACKUP FOLDER : " bkp
+
+
+mv archive.tar $bkp
+
+rm $path2/*.$ext
