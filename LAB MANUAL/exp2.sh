dd1e83e0b4e9da99d94386c3877b581185ff8459
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-10-02 13:21:34 +0530

    experiment 3

--- LAB MANUAL/exp2.sh
+++ LAB MANUAL/exp2.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 # Reading the text from stdin and assigning it to the variable named "myName"
+
 read myName
+
 # Substitute the variable and print the text
+
 echo "Hello ${myName}"
+
+echo "any error text can be here" >&2
