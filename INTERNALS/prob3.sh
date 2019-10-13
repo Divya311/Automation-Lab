63c7058a8dea46361e6803d24dfdb56b8cbb8a59
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-10-13 18:09:18 +0530

    class task 3

---
+++ INTERNALS/prob3.sh
@@ -0,0 +1,10 @@
+#bin/bash
+
+read -e -p "Enter location of archived file : " location
+read -e -p "Enter days before you want to delete : " day
+
+cd $location
+
+rm $(find -mtime +$day -name "*.sh")
+
+
